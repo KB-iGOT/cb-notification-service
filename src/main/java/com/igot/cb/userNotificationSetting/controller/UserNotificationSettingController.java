8ac385df6476ff19f65054c2bab2f19b97a07c72
DeepikaMalav28 <deepika.malav@tarento.com>
2025-06-23 12:05:45 +0530

    User preference (#25)
    * #KB-10272 userPreference add
    
    * change path

--- src/main/java/com/igot/cb/userNotificationSetting/controller/UserNotificationSettingController.java
+++ src/main/java/com/igot/cb/userNotificationSetting/controller/UserNotificationSettingController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/notificationSetting")
+@RequestMapping("/notificationSetting")
 public class UserNotificationSettingController {
 
     @Autowired
