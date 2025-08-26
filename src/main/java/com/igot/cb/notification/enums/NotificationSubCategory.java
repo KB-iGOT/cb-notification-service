02e1cc51a2efa4b6fa3aa59cbc36314bf5863cdf
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-08-26 11:15:59 +0530

    4.8.28 dev (#42)
    * #KB-10604 add more subCategory (#36)
    
    * add more category (#37)
    
    * KB-10604 added global notification modification
    
    * KB-10604 remove springSecurity
    
    * KB-10604 remove unused code
    
    * KB-10604 add category (#40)
    
    ---------
    
    Co-authored-by: DeepikaMalav28 <deepika.malav@tarento.com>
    Co-authored-by: Rajeev Sathish <rajeev.sathish@tarento.com>

--- src/main/java/com/igot/cb/notification/enums/NotificationSubCategory.java
+++ src/main/java/com/igot/cb/notification/enums/NotificationSubCategory.java
@@ -84,8 +84,16 @@ public enum NotificationSubCategory {
     USER_TRANSFER(false),
     CONTENT_SHARE(false),
     TAGGED_COMMENT(false),
-    TAGGED_POST(false);
-
+    TAGGED_POST(false),
+    EVENT_PUBLISHED(false),
+    EVENT_ENROLLED(false),
+    COURSE_PUBLISHED(false),
+    PROGRAM_PUBLISHED(false),
+    LEARN_DISCUSSION_POST_COMMENT(false),
+    LEARN_DISCUSSION_POST_REPLY(false),
+    TRANSFER_UPDATE(false),
+    PROFILE_UPDATE(false),
+    PROFANITY_CHECK(false);
     private boolean shouldClub;
 
     public String messageTemplate() {
--- src/main/java/com/igot/cb/userNotificationSetting/enums/NotificationType.java
+++ src/main/java/com/igot/cb/userNotificationSetting/enums/NotificationType.java
@@ -1,8 +1,5 @@
 package com.igot.cb.userNotificationSetting.enums;
 
 public enum NotificationType {
-    IN_APP,
-//    SMS,
-//    EMAIL,
-//    PUSH
+    IN_APP
 }
