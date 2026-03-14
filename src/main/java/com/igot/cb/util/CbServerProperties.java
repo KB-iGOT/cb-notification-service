9153340361f91ad7138477268cdd557bb0e5f566
tarentomaheshvakkund <mahesh.vakkund@tarento.com>
2026-03-14 15:59:33 +0530

    Removed the Unused attributes for Peer Validation (#85)

--- src/main/java/com/igot/cb/util/CbServerProperties.java
+++ src/main/java/com/igot/cb/util/CbServerProperties.java
@@ -56,12 +56,6 @@ public class CbServerProperties {
   @Value("${peervalidation.list.max.fetch}")
   private int peerValidationListMaxFetch;
 
-  @Value("${kafka.topic.peer.validation.status}")
-  private String kafkaTopicPeerValidationStatus;
-
-  @Value("${kafka.consumer.peer.validation.group-id}")
-  private String kafkaConsumerPeerValidationGroupId;
-
   @Value("${kafka.topic.process.peer.validation.error}")
   private String kafkaTopicPeerValidationError;
 
